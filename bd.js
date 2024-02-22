aad98afda43ac716aa2710afbe5499080c8bcb6f
Robert Diaz <dirobert2000@gmail.com>
2024-02-22 14:04:21 -0500

    Integre el proyecto con Node JS y Express, para mas informacion me preguntan porfa - Robert

---
+++ bd.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    database: "hotelbd",
+    user: "root",
+    password: ""
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Tamos ready!");
+});
+
+module.exports = con;
---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var db = require('./bd.js');
+var app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+
+app.post('/login', function(peticion,respuesta) {
+    var usuario = peticion.body.correo;
+    var password = peticion.body.contra;
+    console.log(usuario);
+    console.log(password);
+    db.query('SELECT * FROM usuario WHERE usuario = ? AND password = ?', [usuario, password], function(error, resultado) {
+        if (resultado.length > 0) {
+            respuesta.redirect('/home');
+        } else {
+            respuesta.send('Usuario o contraseña incorrectos');
+        }
+        respuesta.end();
+    });
+});
+
+
+
+app.get('/home', function(peticion,respuesta) {
+    respuesta.sendFile(__dirname + '/public/index.html');
+});
+app.listen(3000, function(peticion,respuesta) {
+    console.log('server running on port 3000');
+});
