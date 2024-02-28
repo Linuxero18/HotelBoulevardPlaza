8547cc3fdbb75078fd0ab048dbec480a03d66253
Robert Diaz <dirobert2000@gmail.com>
2024-02-28 09:52:59 -0500

    Terminada la funcion de buscar reclamos

--- public/scripts/script_reclamos_busqueda.js
+++ public/scripts/script_reclamos_busqueda.js
@@ -18,25 +18,60 @@
 //     }
 // }
 
-function fetchData() {
-    const idNumber = document.getElementById('numero_reclamo').value;
-    if (idNumber) {
-        // Llamada a la API del servidor Node.js
-        fetch(`/search?id=${idNumber}`)
-            .then(response => {
-                if (!response.ok) {
-                    displayError();
-                }
-                return response.json();
-            })
-            .then(data => {
-                displayResults(data);
-            })
-            .catch(error => {
+// function fetchData() {
+//     const idNumber = document.getElementById('numero_reclamo').value;
+//     if (idNumber) {
+//         // Llamada a la API del servidor Node.js
+//         fetch(`/search?id=${idNumber}`)
+//             .then(response => {
+//                 if (!response.ok) {
+//                     displayError();
+//                 }
+//                 return response.json();
+//             })
+//             .then(data => {
+//                 displayResults(data);
+//             })
+//             .catch(error => {
                 
-                console.error('Error:', error);
-            });
+//                 console.error('Error:', error);
+//             });
+//     }
+// }
+
+
+//Este es el metodo que captura los datos del formulario y los envia al servidor
+function fetchData() {
+    let params = {};
+    if (document.getElementById("opcion1").checked) {
+        params.tipo_documento = document.getElementById("tipo_documento").value;
+        params.numero_documento = document.getElementById("numero_documento").value;
+        params.apellidos = document.getElementById("apellidos").value;
+    } else if (document.getElementById("opcion2").checked) {
+        params.numero_reclamo = document.getElementById("numero_reclamo").value;
     }
+
+    console.log(params);
+
+    fetch('/search', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params),
+    })
+        .then(response => {
+            if (!response.ok) {
+                displayError();
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayResults(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
 // function displayResults(data) {
@@ -65,6 +100,8 @@ function fetchData() {
 //     resultado.innerHTML= fecha;
 // }
 
+//Este es el metodo que muestra los datos que se obtuvieron del servidor
+//Estoy utilizando sweetalert2 para mostrar los mensajes
 function displayResults(data) {
     let fecha = ((data[0].fecha_reclamo).split("T")[0]);
     fecha = fecha.split("-").reverse().join("/");
@@ -94,4 +131,21 @@ function displayError() {
         text: "No se encontraron datos",
         confirmButtonColor: "#48a04b"
     });
+}
+
+
+//Este metodo maneja si los campos del formulario estan habilitados o no
+function manejarCampos() {
+    let opcion1Checked = document.getElementById("opcion1").checked;
+    document.getElementById("numero_reclamo").disabled = opcion1Checked;
+    document.getElementById("numero_reclamo").value = "";
+
+    let opcion2Checked = document.getElementById("opcion2").checked;
+    document.getElementById("tipo_documento").disabled = opcion2Checked;
+    document.getElementById("numero_documento").disabled = opcion2Checked;
+    document.getElementById("apellidos").disabled = opcion2Checked;
+    document.getElementById("tipo_documento").value = "DNI";
+    document.getElementById("numero_documento").value = "";
+    document.getElementById("apellidos").value = "";
+    
 }
